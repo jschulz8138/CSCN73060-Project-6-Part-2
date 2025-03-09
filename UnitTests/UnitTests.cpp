56f557fdde0c2e9b07544c2bc778f2556155ca4a
Justin Schulz <Jschulz@conestogac.on.ca>
2025-03-09 19:05:28 -0400

    Added UnitTests solution

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(Packet)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(Date)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(TelemetryData)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
