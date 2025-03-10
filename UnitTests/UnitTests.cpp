5347e66256d7c9294dad8fbb8fda5c43f00c2b27
Justin Schulz <Jschulz@conestogac.on.ca>
2025-03-09 20:23:04 -0400

    linker errors are still present

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,35 +1,44 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-
+////#include "../Shared/GeneratePacket.h"
+////#include "../Shared/SendPacket.h"
+////#include "../Shared/EndPacket.h"
+////#include "../Shared/AckPacket.h"
+////#include "../Shared/PacketFactory.h"
+#include "../Shared/Date.h"
+//
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+//
 namespace UnitTests
 {
-	TEST_CLASS(Packet)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
+	//TEST_CLASS(PacketUnitTests)
+	//{
+	//public:
 
-		}
-	};
-	TEST_CLASS(Date)
-	{
-	public:
+	//	TEST_METHOD(TestMethod1)
+	//	{
 
-		TEST_METHOD(TestMethod1)
-		{
-
-		}
-	};
-	TEST_CLASS(TelemetryData)
+	//	}
+	//};
+	TEST_CLASS(DateUnitTests)
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestConstructorValidDate)
 		{
-
+			const char* dateString = "3_3_2023 14:53:2";
+			Date date(dateString);
+			std::string expectedDate = "03_03_2023 14:53:02";
+			Assert::AreEqual(std::string(date), expectedDate);
 		}
 	};
+	//TEST_CLASS(TelemetryDataUnitTests)
+	//{
+	//public:
+
+	//	TEST_METHOD(TestMethod1)
+	//	{
+
+	//	}
+	//};
 }
